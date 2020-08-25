10b61530412d0c17b26b10e571bf182b7c4de795
SongYongsoo <syssys131@naver.com>
2020-08-25 21:22:51 +0900

    ~#2.1

--- App.js
+++ App.js
@@ -1,26 +1,49 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Alert } from "react-native";
+import Loading from "./Loading";
+import * as Location from "expo-location";
+import axios from "axios";
+import Weather from "./Weather";
 
-export default function App() {
-    return (
-        <View style={styles.container}>
-            <View style={styles.yellowView}><Text></Text></View>
-            <View style={styles.blueView}><Text></Text></View>
-            <StatusBar style="auto" />
-        </View>
-    );
+const API_KEY = "77366365637ced520d4188028d414297";
+
+export default class extends React.Component {
+    state = {
+        isLoading: true,
+    };
+    getWeather = async (latitude, longitude) => {
+        const { data:{
+            main:{temp},
+            weather
+        } } = await axios.get(
+            `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+        );
+        this.setState({
+            isLoading: false,
+            condition: weather[0].main,
+            temp,
+        });
+    };
+    getLocation = async () => {
+        try {
+            await Location.requestPermissionsAsync();
+            const {
+                coords: { latitude, longitude },
+            } = await Location.getCurrentPositionAsync();
+            this.getWeather(latitude, longitude);
+        } catch (error) {
+            Alert.alert("Can't find you.", "So sad");
+        }
+    };
+    componentDidMount() {
+        this.getLocation();
+    }
+    render() {
+        const { isLoading, temp, condition } = this.state;
+        return isLoading ? (
+            <Loading></Loading>
+        ) : (
+            <Weather temp={Math.round(temp)} condition={condition}></Weather>
+        );
+    }
 }
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    yellowView: {
-        flex: 1,
-        backgroundColor: "yellow",
-    },
-    blueView: {
-        flex: 3,
-        backgroundColor: "blue",
-    },
-});
